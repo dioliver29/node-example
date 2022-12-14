943220bed2c1eccdda9e1d06a56d9ed48d982635
Diana Stikheeva <d.stikheeva@gmail.com>
2022-12-15 01:53:16 +0500

    init: homework

---
+++ src/tale.js
@@ -0,0 +1,55 @@
+
+const characterName = kolobok('дедушка', 'заяц', 'лиса')
+
+console.log(kolobok('дедушка'))
+console.log(kolobok('заяц'))
+console.log(kolobok('лиса'))
+console.log(kolobok('кот'))
+
+
+function kolobok (characterName) {
+    switch(characterName) {
+        case 'дедушка':
+            return 'Я от дедушки ушёл'
+            break;
+        case 'заяц':
+            return 'Я от бабушки ушёл'
+            break;
+        case 'лиса':
+            return 'Меня съели'
+            break;
+        default:
+            return 'неизвестно'
+    }
+}
+
+
+const dedMoroz = 'Дед Мороз';
+const snegurochka = 'Снегурочка';
+const phrase1 = `${dedMoroz}! ${dedMoroz}! ${dedMoroz}!`;
+const phrase2 = `${snegurochka}! ${snegurochka}! ${snegurochka}!`;
+const names = newYear(dedMoroz,snegurochka)
+
+function newYear(names) {
+    if (names === dedMoroz) {
+        return phrase1;
+    } else if (names === snegurochka) {
+        return phrase2;
+    } else {
+        return 'новый год';
+    }
+}
+
+console.log(newYear(dedMoroz))
+console.log(newYear(snegurochka))
+
+
+/* function diffTwoNumber(a, b) {
+    if (isNaN(a) === false) {
+        if (isNaN(b) === false) {
+            console.log(a - b);
+        }
+    } else {
+        console.log('a and b must have type number');
+    }
+} */
